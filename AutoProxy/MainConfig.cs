5e68f8c6d709f8e6106529b2b032d38e00a2620b
Thomas Bell <tom.aus@outlook.com>
2016-09-04 19:30:44 +0800

    Initial UI Design

--- AutoProxy/MainConfig.cs
+++ AutoProxy/MainConfig.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AutoProxy
 {
-    public partial class Form1 : Form
+    public partial class MainConfig : Form
     {
-        public Form1()
+        public MainConfig()
         {
             InitializeComponent();
         }
